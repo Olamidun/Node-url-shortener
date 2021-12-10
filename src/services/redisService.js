43ea0ee28f50c72f9d6140184fd521474c867a92
Olamidun <kolapoolamidun@gmail.com>
2021-12-10 15:57:17 +0100

    switched to redis 4.0.0

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -1,6 +1,10 @@
 const redis = require('redis');
 
-const client = redis.createClient(process.env.REDIS_URL);
+const client = redis.createClient({
+  host: 'ec2-34-236-230-40.compute-1.amazonaws.com',
+  port: 8700,
+  password: 'pf05602cc4bc02a1a3350c890d2cfb55ab45a27b1133a52b9153530da349c771e',
+});
 client.connect();
 
 client.on('Connect', () => {
