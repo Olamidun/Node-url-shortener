8702be4409907162eb26205c4f8fa38718b80c41
Abdulmateen Tairu <tay2druh@gmail.com>
2021-12-11 10:46:18 +0100

    A couple of fixes on redis

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -1,12 +1,25 @@
 const redis = require('redis');
 
-const redisConf = {
-  host: 'ec2-54-161-13-227.compute-1.amazonaws.com',
-  port: 8980,
-  pass: 'p866296af92e979de3e5e2f7fc1249840fc2ec4e0d818fa65384ece1974d9307a',
+module.exports = () => {
+  const redisConf = {
+    url: process.env.REDIS_URL,
+  };
+
+  const client = redis.createClient({
+    url: redisConf.url,
+  });
+
+  client.on('Connect', () => {
+    console.log('Connected');
+  });
+
+  client.on('ready', () => {
+    console.log('Connected to redis and ready for use');
+  });
+
+  client.on('error', (error) => {
+    console.log(error.message);
+  });
+
+  return client;
 };
-
-const client = redis.createClient();
-// client.connect();
-
-module.exports = client;
